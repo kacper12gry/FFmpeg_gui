21feea472703cbc725163f7023e20be6820e39ac
kacper12gry <91696743+kacper12gry@users.noreply.github.com>
2025-02-09 10:00:41 +0100

    Update process_manager.py

--- v2/stable_prawie/process_manager.py
+++ v2/stable_prawie/process_manager.py
@@ -39,7 +39,7 @@ class ProcessManager:
 
     def run_mkvmerge(self, mkv_file, subtitle_file, font_folder):
         final_output_file = mkv_file.replace(".mkv", "_remux.mkv")
-        font_files = ' '.join([f'--attach-file "{os.path.join(font_folder, font)}"' for font in os.listdir(font_folder) if font.endswith(('.ttf', '.otf', '.woff', '.woff2'))])
+        font_files = ' '.join([f'--attach-file "{os.path.join(font_folder, font)}"' for font in os.listdir(font_folder) if font.endswith(('.ttf', '.otf', '.woff', '.woff2', '.OTF', 'TTF'))])
         command = f'mkvmerge -o "{final_output_file}" --no-subtitles --no-buttons --no-track-tags --no-chapters --no-attachments "{mkv_file}" --language 0:pol --track-name 0:"FrixySubs" "{subtitle_file}" --attachment-mime-type application/x-truetype-font {font_files}'
         self.output_window.clear()  # Clear the output window before starting a new process
         self.process = QProcess()
@@ -54,7 +54,7 @@ class ProcessManager:
 
     def run_mkvmerge_ffmpeg(self, mkv_file, subtitle_file, font_folder):
         final_output_file = mkv_file.replace(".mkv", "_remux.mkv")
-        font_files = ' '.join([f'--attach-file "{os.path.join(font_folder, font)}"' for font in os.listdir(font_folder) if font.endswith(('.ttf', '.otf', '.woff', '.woff2'))])
+        font_files = ' '.join([f'--attach-file "{os.path.join(font_folder, font)}"' for font in os.listdir(font_folder) if font.endswith(('.ttf', '.otf', '.woff', '.woff2', '.OTF', 'TTF'))])
         command = f'mkvmerge -o "{final_output_file}" --no-subtitles --no-buttons --no-track-tags --no-chapters --no-attachments "{mkv_file}" --language 0:pol --track-name 0:"FrixySubs" "{subtitle_file}" --attachment-mime-type application/x-truetype-font {font_files}'
         self.output_window.clear()  # Clear the output window before starting a new process
         self.process = QProcess()
@@ -71,17 +71,17 @@ class ProcessManager:
         output_file = mkv_file.replace("_remux.mkv" if is_final else ".mkv", "_hardsub.mp4")
         if self.selected_ffmpeg_script == 1:
             command = [
-                "ffmpeg", "-i", f'"{mkv_file}"', "-vf", f'format=yuv420p,subtitles=\'{mkv_file}\'',
+                "ffmpeg", "-i", mkv_file, "-vf", f'format=yuv420p,subtitles=\'{mkv_file}\'',
                 "-map_metadata", "-1", "-movflags", "faststart", "-c:v", "libx264", "-profile:v", "main",
                 "-level:v", "4.0", "-preset", "veryfast", "-crf", "16", "-maxrate", "20M", "-bufsize", "25M",
-                "-x264-params", "colormatrix=bt709", "-c:a", "copy", f'"{output_file}"'
+                "-x264-params", "colormatrix=bt709", "-c:a", "copy", output_file
             ]
         else:
             command = [
-                "ffmpeg", "-y", "-vsync", "0", "-hwaccel", "cuda", "-i", f'"{mkv_file}"', "-vf", f'subtitles=\'{mkv_file}\'',
+                "ffmpeg", "-y", "-vsync", "0", "-hwaccel", "cuda", "-i", mkv_file, "-vf", f'subtitles=\'{mkv_file}\'',
                 "-c:a", "copy", "-c:v", "h264_nvenc", "-preset", "p2", "-tune", "1", f'-b:v {self.gpu_bitrate}M', "-bufsize", "15M", "-maxrate", "15M",
                 "-qmin", "0", "-g", "250", "-bf", "3", "-b_ref_mode", "middle", "-temporal-aq", "1", "-rc-lookahead", "20",
-                "-i_qfactor", "0.75", "-b_qfactor", "1.1", f'"{output_file}"'
+                "-i_qfactor", "0.75", "-b_qfactor", "1.1", output_file
             ]
         self.output_window.clear()  # Clear the output window before starting a new process
         self.process = QProcess()
@@ -89,7 +89,7 @@ class ProcessManager:
         self.process.readyRead.connect(self.update_output)
         self.process.finished.connect(self.process_next_in_queue)
         self.process.finished.connect(self.mark_task_as_done)
-        self.process.start(" ".join(command))
+        self.process.start(command[0], command[1:])
         self.task_list.setCurrentRow(0)
         self.task_list.currentItem().setText(f"Wideo: {mkv_file} (ffmpeg) - Skrypt: {'CPU' if self.selected_ffmpeg_script == 1 else f'GPU (Nvidia) - Bitrate: {self.gpu_bitrate}M'}")
         if self.debug_mode:
